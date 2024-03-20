beb7c8fbb879d7aed5366f90e599d51af5fc1950
hibernon rodrigues <hibernon73@gmail.com>
2024-03-20 14:49:35 -0300

    2 commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
+
+    $('form').on('submit',function(e){
+    e.preventDefault()    
+    })
+
     $('#telefone').mask('(00) 00000-0000')
+
+    $('#CPF').mask('000.000.000-00')
+
+    $('#CEP').mask('00000-000')
 })
